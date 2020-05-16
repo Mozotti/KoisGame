14b1b6444f5f7eabda929c32cf6eff685fa04874
Mozotti <morotti@live.ru>
2020-05-16 14:05:20 -0300

    Unity Version Updated

---
+++ Assets/Editor/GroupGameObjects.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class GroupGameObjects
+    {
+        [MenuItem("GameObject/Group GameObjects %g")]
+        private static void GroupSelected()
+        {
+            if (!Selection.activeTransform) return;
+
+            GameObject Group = new GameObject
+            {
+                name = "Group"
+            };
+
+            Undo.RegisterCompleteObjectUndo(Group, "Group GameObjects");
+
+            Group.transform.SetParent(Selection.activeTransform.parent, false);
+
+            foreach (var transform in Selection.transforms)
+            {
+                Undo.SetTransformParent(transform, Group.transform, "Group GameObjects");
+            }
+
+            Selection.activeObject = Group;
+        }
+    }
+}
